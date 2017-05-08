fb8f36e371f3fc5765f50d5d7fd28ef90e9f1cdf
svc-student <svc-student@student.directory.jccc>
2017-05-08 11:20:32 -0500

    Finished

---
+++ Final-master/Assets/Editor/EditStitch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EditStitch : EditorWindow {
+
+    public Stitch myStitch;
+
+    public void Init(Stitch stitch)
+    {
+        myStitch = stitch;
+    }
+
+    private void OnGUI()
+    {
+        myStitch.stitchName = EditorGUILayout.TextField("Stitch Name", myStitch.stitchName);
+
+        myStitch.summary = EditorGUILayout.TextField("Summary", myStitch.summary);
+
+        myStitch.background = (Sprite)EditorGUILayout.ObjectField("Background",myStitch.background, typeof(Sprite), true) as Sprite;
+
+        ScriptableObject stitch = myStitch;
+        SerializedObject so = new SerializedObject(stitch);
+        SerializedProperty performers = so.FindProperty("performers");
+        EditorGUILayout.PropertyField(performers, true);
+        so.ApplyModifiedProperties();
+
+        SerializedProperty dialogs = so.FindProperty("dialogs");
+        EditorGUILayout.PropertyField(dialogs, true);
+        so.ApplyModifiedProperties();
+
+        SerializedProperty yarns = so.FindProperty("yarns");
+        EditorGUILayout.PropertyField(yarns, true);
+        so.ApplyModifiedProperties();
+
+        myStitch.status = (Stitch.stitchStatus)EditorGUILayout.EnumPopup("Status", myStitch.status);
+
+    }
+}
---
+++ Final-master/Assets/Editor/StitchNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class StitchNode : NodeBaseClass {
+
+    public Stitch myStitch;
+
+    public StitchNode(Rect r, int ID, Stitch stitch) : base(r,ID)
+    {
+        myStitch = stitch;
+    }
+
+    public override void DrawGUI(int winID)
+    {
+        if (GUILayout.Button("Edit"))
+        {
+            EditStitch editWindow = EditorWindow.GetWindow<EditStitch>();
+
+            editWindow.Init(myStitch);
+        }
+        base.BaseDraw();
+    }
+
+    public override void AttachComplete(NodeBaseClass winID)
+    {
+        base.linkedNodes.Add(winID);
+    }
+}
